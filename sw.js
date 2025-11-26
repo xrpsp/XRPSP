606dead32924a65f6ad9381d9cbf634e57fa08da
xrpsp <215057929+xrpsp@users.noreply.github.com>
2025-11-26 02:10:03 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
-    "domain": "couphaithuph.net",
-    "zoneId": 9944605
+    "domain": "3nbf4.com",
+    "zoneId": 10237043
 }
 self.lary = ""
-importScripts('https://couphaithuph.net/act/files/service-worker.min.js?r=sw')
+importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
