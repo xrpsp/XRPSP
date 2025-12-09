7c8ac1cc48af206024f8d5aa1b7389d7a007e54c
xrpsp <215057929+xrpsp@users.noreply.github.com>
2025-12-09 16:27:24 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
-    "domain": "3nbf4.com",
-    "zoneId": 10279212
+    "domain": "eechicha.com",
+    "zoneId": 9943778
 }
 self.lary = ""
-importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
+importScripts('https://eechicha.com/act/files/service-worker.min.js?r=sw')
